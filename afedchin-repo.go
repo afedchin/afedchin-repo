fe87ed31d400f204eb138ee4e8a95e5eeb850040
Anton Fedchin <anightik@gmail.com>
2016-12-28 16:07:31 +0300

    cosmetics.

--- afedchin-repo.go
+++ afedchin-repo.go
@@ -9,11 +9,9 @@ import (
 	"encoding/xml"
 	"fmt"
 	"html/template"
-	"io"
 	"io/ioutil"
 	"net/http"
 	"os"
-	"path"
 	"sort"
 	"strings"
 	"time"
@@ -23,9 +21,12 @@ import (
 	"github.com/mcuadros/go-version"
 )
 
-var repositories = []string{"afedchin/xbmctorrent", "afedchin/antizapret"}
+var repositories = []string{
+	"afedchin/xbmctorrent",
+	"afedchin/antizapret",
+}
 
-var IndexTemplate, _ = template.New("index").Parse(
+var indexTemplate, _ = template.New("index").Parse(
 	`<html>
     <head><title>Index</title></head>
     <body>
@@ -81,17 +82,17 @@ type Release struct {
 	Assets          []ReleaseAsset `json:"assets"`
 }
 
-type XBMCAddon struct {
+type KodiAddon struct {
 	ID       string `xml:"id,attr"`
 	Version  string `xml:"version,attr"`
 	XMLBody  string
 	Releases []Release
 }
 
-var addons map[string]XBMCAddon
+var addons map[string]KodiAddon
 
-func reloadAddons(repos []string) map[string]XBMCAddon {
-	tmpAddons := map[string]XBMCAddon{}
+func reloadAddons(repos []string) map[string]KodiAddon {
+	tmpAddons := map[string]KodiAddon{}
 	for _, repo := range repos {
 		releaseURL := fmt.Sprintf("https://api.github.com/repos/%s/releases", repo)
 		response, err := http.Get(releaseURL)
@@ -107,7 +108,7 @@ func reloadAddons(repos []string) map[string]XBMCAddon {
 		})
 
 		var addonxml Content
-		var addon XBMCAddon
+		var addon KodiAddon
 
 		lastRelease := releases[0]
 		addonURL := fmt.Sprintf("https://api.github.com/repos/%s/contents/addon.xml.tpl?ref=%s", repo, lastRelease.TagName)
@@ -147,85 +148,6 @@ func reloadAddons(repos []string) map[string]XBMCAddon {
 	return tmpAddons
 }
 
-func assertError(err error) {
-	if err != nil {
-		os.Exit(1)
-	}
-}
-
-func createIfNotExist(dir string) {
-	if _, err := os.Stat(dir); os.IsNotExist(err) {
-		os.MkdirAll(dir, os.ModePerm)
-	}
-}
-
-func overWriteFile(filePath string) *os.File {
-	file, err := os.OpenFile(filePath, os.O_CREATE|os.O_RDWR|os.O_TRUNC, os.ModePerm)
-	assertError(err)
-	return file
-}
-
-func generateRepo(repos []string, baseDir string) {
-	addons = reloadAddons(repos)
-
-	if _, err := os.Stat(baseDir); os.IsNotExist(err) {
-		os.MkdirAll(baseDir, os.ModePerm)
-	}
-
-	files := make(map[string]string)
-	for _, addon := range addons {
-		addonDir := path.Join(baseDir, addon.ID)
-		for _, asset := range addon.Releases[0].Assets {
-			if strings.HasSuffix(asset.Name, addon.Version[1:]+".zip") {
-				files[addon.ID] = asset.Name
-
-				filepath := path.Join(addonDir, asset.Name)
-				// ignore exists files
-				if _, err := os.Stat(filepath); err == nil {
-					continue
-				}
-				createIfNotExist(addonDir)
-				file := overWriteFile(filepath)
-
-				response, err := http.Get(asset.DownloadURL)
-				assertError(err)
-
-				_, err = io.Copy(file, response.Body)
-				assertError(err)
-
-				file.Close()
-				response.Body.Close()
-			}
-		}
-		addonxml := path.Join(addonDir, "addon.xml")
-		file := overWriteFile(addonxml)
-		file.WriteString("<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?>")
-		file.WriteString(addon.XMLBody)
-		file.Close()
-	}
-
-	f := overWriteFile(path.Join(baseDir, "index.html"))
-	IndexTemplate.Execute(f, files)
-	f.Close()
-
-	f = overWriteFile(path.Join(baseDir, "addons.xml"))
-
-	h := md5.New()
-	fmt.Fprintf(f, "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<addons>")
-	fmt.Fprintf(h, "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<addons>")
-	for _, addon := range addons {
-		fmt.Fprintf(f, addon.XMLBody)
-		fmt.Fprintf(h, addon.XMLBody)
-	}
-	fmt.Fprintf(f, "</addons>")
-	fmt.Fprintf(h, "</addons>")
-	f.Close()
-
-	f = overWriteFile(path.Join(baseDir, "addons.xml.md5"))
-	fmt.Fprintf(f, "%x", h.Sum(nil))
-	f.Close()
-}
-
 func repoMuxer(repos []string) *mux.Router {
 	router := mux.NewRouter()
 
@@ -245,7 +167,7 @@ func repoMuxer(repos []string) *mux.Router {
 				}
 			}
 		}
-		IndexTemplate.Execute(w, files)
+		indexTemplate.Execute(w, files)
 	})
 
 	router.HandleFunc("/addons.xml", func(w http.ResponseWriter, r *http.Request) {
@@ -268,31 +190,43 @@ func repoMuxer(repos []string) *mux.Router {
 
 	router.HandleFunc("/{addon_id}/changelog-{version}.txt", func(w http.ResponseWriter, r *http.Request) {
 		vars := mux.Vars(r)
-		addon := addons[vars["addon_id"]]
-		for _, release := range addon.Releases {
-			fmt.Fprintf(w, "%s\n-------\n%s\n\n", release.Name, release.Body)
+		if addon, ok := addons[vars["addon_id"]]; ok {
+			for _, release := range addon.Releases {
+				fmt.Fprintf(w, "%s\n-------\n%s\n\n", release.TagName, release.Body)
+			}
 		}
 	})
 
-	/*router.HandleFunc("/{addon_id}/fanart.jpg", func(w http.ResponseWriter, r *http.Request) {
-	      vars := mux.Vars(r)
-	      addon := addons[vars["addon_id"]]
-	      http.Redirect(w, r, addon.Releases[0].AssetDownloadURL("fanart.jpg"), 302)
-	  })
+	router.HandleFunc("/{addon_id}/fanart.jpg", func(w http.ResponseWriter, r *http.Request) {
+		vars := mux.Vars(r)
+		if addon, ok := addons[vars["addon_id"]]; ok {
+			for _, asset := range addon.Releases[0].Assets {
+				if strings.Compare(asset.Name, "fanart.jpg") == 0 {
+					http.Redirect(w, r, asset.DownloadURL, 302)
+				}
+			}
+		}
+	})
 
-	  router.HandleFunc("/{addon_id}/icon.png", func(w http.ResponseWriter, r *http.Request) {
-	      vars := mux.Vars(r)
-	      addon := addons[vars["addon_id"]]
-	      http.Redirect(w, r, addon.Releases[0].AssetDownloadURL("icon.png"), 302)
-	  })*/
+	router.HandleFunc("/{addon_id}/icon.png", func(w http.ResponseWriter, r *http.Request) {
+		vars := mux.Vars(r)
+		if addon, ok := addons[vars["addon_id"]]; ok {
+			for _, asset := range addon.Releases[0].Assets {
+				if strings.Compare(asset.Name, "icon.png") == 0 {
+					http.Redirect(w, r, asset.DownloadURL, 302)
+				}
+			}
+		}
+	})
 
 	router.HandleFunc("/{addon_id}/{file}.zip", func(w http.ResponseWriter, r *http.Request) {
 		vars := mux.Vars(r)
-		addon := addons[vars["addon_id"]]
-		file := fmt.Sprintf("%s.zip", vars["file"])
-		for _, asset := range addon.Releases[0].Assets {
-			if strings.Compare(asset.Name, file) == 0 {
-				http.Redirect(w, r, asset.DownloadURL, 302)
+		if addon, ok := addons[vars["addon_id"]]; ok {
+			file := fmt.Sprintf("%s.zip", vars["file"])
+			for _, asset := range addon.Releases[0].Assets {
+				if strings.Compare(asset.Name, file) == 0 {
+					http.Redirect(w, r, asset.DownloadURL, 302)
+				}
 			}
 		}
 	})
